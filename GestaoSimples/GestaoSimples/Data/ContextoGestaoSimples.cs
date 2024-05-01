ce7b50d7cd7732aaada6bb0a9f11344ba80d3252
Júlio César <julio10as@gmail.com>
2024-05-01 03:30:08 -0300

    Adicionado configuração inicial do DbContext e DB GestaoSimples criado com sucesso

---
+++ GestaoSimples/GestaoSimples/Data/ContextoGestaoSimples.cs
@@ -0,0 +1,15 @@
+﻿using GestaoSimples.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoSimples.Data
+{
+    public class ContextoGestaoSimples : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=CRUZETOBOOK\SQLEXPRESS;Initial Catalog=GestaoSimples;Integrated Security=true;");
+        }
+    }
+}
