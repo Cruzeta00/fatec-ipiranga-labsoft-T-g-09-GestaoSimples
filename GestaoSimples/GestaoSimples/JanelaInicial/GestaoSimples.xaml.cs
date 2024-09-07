dfbb8b9a0478965c72855799392c398246ff35db
Júlio César <julio10as@gmail.com>
2024-09-07 20:03:45 -0300

    Logo adicionado

--- GestaoSimples/GestaoSimples/JanelaInicial/GestaoSimples.xaml.cs
+++ GestaoSimples/GestaoSimples/JanelaInicial/GestaoSimples.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +12,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +30,9 @@ namespace GestaoSimples
         public GestaoSimples()
         {
             this.InitializeComponent();
+
+            this.Title = "Gestão Simples";
+            this.AppWindow.SetIcon(Path.GetFullPath("Recursos/Logo.ico"));
         }
     }
 }
